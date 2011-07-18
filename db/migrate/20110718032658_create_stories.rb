459b67de81cf08a151dea37adb4da0cbcf9b0b2e
Christian Höltje <docwhat@gerf.org>
2011-07-17 23:45:49 -0400

    Added stories

---
+++ db/migrate/20110718032658_create_stories.rb
@@ -0,0 +1,11 @@
+class CreateStories < ActiveRecord::Migration
+  def change
+    create_table :stories do |t|
+      t.string :title
+      t.string :author
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
