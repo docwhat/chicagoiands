f4aad5ab223a73a391fbbf0565db44f166ca02c6
Christian Höltje <docwhat@gerf.org>
2011-08-03 21:54:02 -0400

    fixed update script

--- old-site/update.sh
+++ old-site/update.sh
@@ -4,7 +4,7 @@ set -eux
 
 cd "$(dirname "$0")"
 
-curl -n https://zope.gerf.org/chicagoiands/stories/export_xml >| stories.xmx
+curl -n https://zope.gerf.org/chicagoiands/stories/export_xml >| stories.xml
 curl -n https://zope.gerf.org/chicagoiands/schedule/export_xml >| schedule.xml
 
 # EOF
