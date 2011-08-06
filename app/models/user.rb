d595c92eeaf3868ead654a6472ee19ec7ce36309
Christian Höltje <docwhat@gerf.org>
2011-08-05 22:21:29 -0400

    got rid of emails for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ActiveRecord::Base
-  attr_accessible :logon, :email, :password, :password_confirmation
+  attr_accessible :logon, :password, :password_confirmation
   has_secure_password
 
   validates_presence_of :logon
   validates_uniqueness_of :logon
-
-  validates_presence_of :email
-  validates_uniqueness_of :email
 
   validates_presence_of :password, :on => :create
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110806011542) do
+ActiveRecord::Schema.define(:version => 20110806021047) do
 
   create_table "meetings", :force => true do |t|
     t.string   "title"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(:version => 20110806011542) do
 
   create_table "users", :force => true do |t|
     t.string   "logon"
-    t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
