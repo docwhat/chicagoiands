587fc09d2123effb67f94fd6847d366e95655a05
Christian Höltje <docwhat@gerf.org>
2011-08-05 23:04:26 -0400

    Trying to fix PGSQL error

--- db/migrate/20110804024251_create_users.rb
+++ db/migrate/20110804024251_create_users.rb
@@ -2,7 +2,6 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :logon
-      t.string :email
       t.string :password_digest
 
       t.timestamps
--- db/migrate/20110806021047_remove_users_emails.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUsersEmails < ActiveRecord::Migration
-  def up
-    remove_column :users, :email
-  end
-
-  def down
-    add_column :users, :email, :string
-  end
-end
