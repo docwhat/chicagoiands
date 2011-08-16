769d0e92d83ae5ec70ae218e19d5694f127ce126
Christian Höltje <docwhat@gerf.org>
2011-08-15 23:35:15 -0400

    Season test was wrong
    The Season test wasn't properly mocking the Date object to
    test the current_season. I split the test into two parts: before the season
    cut off and after.

--- spec/lib/season_spec.rb
+++ spec/lib/season_spec.rb
@@ -2,16 +2,24 @@ require 'spec_helper'
 require 'date'
 
 describe Season do
-  it "should have a current season" do
-    year = 2010
-    Date.any_instance.stubs(:today).returns(Date.new(year, 06, 06)).at_least_once
+  it "should have a current season (pre-cutoff)" do
+    year = 2000
+    Date.expects(:today).returns(Date.new(year, 6, 6)).at_least_once
+    current = Season.current_season
+    current.start.year.should == year - 1
+    current.stop.year.should == year
+  end
+
+  it "should have a current season (post-cutoff)" do
+    year = 2001
+    Date.expects(:today).returns(Date.new(year, 9, 6)).at_least_once
     current = Season.current_season
     current.start.year.should == year
     current.stop.year.should == year + 1
   end
 
   it "should have a stop year one more than the stop year" do
-    year = 1999
+    year = 2002
     season = Season.new year
     season.year.should == year
     season.start.year.should == year
@@ -19,7 +27,7 @@ describe Season do
   end
 
   it "should start on the 15th of August" do
-    year = 1999
+    year = 2003
     season = Season.new year
     season.start.should == Date.new(year, 8, 15)
     season.stop.should == Date.new(year + 1, 8, 15)
