00bce5f48fe00b5e71c62e84bf246357b31a8acf
Christian Höltje <docwhat@gerf.org>
2011-12-01 02:26:48 -0500

    Changed rate for times.rb

--- times.rb
+++ times.rb
@@ -4,6 +4,8 @@ require 'pathname'
 
 input = Pathname.new(__FILE__).dirname + 'times.md'
 
+RATE = 80
+
 time = 0
 input.open('r') do |f|
   f.each do |line|
@@ -14,6 +16,6 @@ input.open('r') do |f|
 end
 
 puts "Hours: #{time}"
-puts "Cost:  $#{time * 250}"
+puts "Cost:  $#{time * RATE} @ $#{RATE}/hour"
 
 # EOF
