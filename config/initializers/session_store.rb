7ddaa17fe99b8a33ce291257f204a611f28fd524
Christian Höltje <docwhat@gerf.org>
2012-03-10 15:28:23 -0500

    Hide the secret token (d'oh)

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-Chicagoiands::Application.config.session_store :cookie_store, key: '_chicagoiands_session'
+Chicagoiands::Application.config.session_store :cookie_store,
+  key: '_chicagoiands_session',
+#  secure: Rails.env == 'production', # Only send cookie over SSL when in production mode
+  httponly: true, # Don't allow Javascript to access the cookie (mitigates cookie-based XSS exploits)
+  expire_after: 60.minutes
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
